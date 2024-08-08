965fb7e79f6683d0924e235c0cf924f083dd9937
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-08-09 01:56:44 +0300

    first commit

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,42 +4,54 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Pepperoni checkbox
 test("checkbox is initially unchecked", () => {
   render(<App />);
-
   const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
   expect(addPepperoni).not.toBeChecked();
+});
+
+test("clicking the Place Order button displays a thank you message", () => {
+  render(<App />);
+  userEvent.click(screen.getByRole("button", { name: /submit order/i }));
+  expect(screen.getByText(/thanks for your order!/i)).toBeInTheDocument();
 });
 
 test("checkbox appears as checked when user clicks it", () => {
   render(<App />);
-
   const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
   userEvent.click(addPepperoni);
   expect(addPepperoni).toBeChecked();
 });
 
 test("checkbox appears as unchecked when user clicks a second time", () => {
   render(<App />);
-
   const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
   userEvent.click(addPepperoni);
-
   expect(addPepperoni).toBeChecked();
-
   userEvent.click(addPepperoni);
-
   expect(addPepperoni).not.toBeChecked();
 });
 
-// Size select element
+test("select Size dropdown displays the user's selected value", () => {
+  render(<App />);
+  const selectSize = screen.getByLabelText(/select size/i);
+  userEvent.selectOptions(selectSize, "medium");
+  expect(selectSize).toHaveDisplayValue("Medium");
+  userEvent.selectOptions(selectSize, "large");
+  expect(selectSize).toHaveDisplayValue("Large");
+});
 
-// "Your Selection" text
+test("selecting options updates the 'Your selection' message", () => {
+  render(<App />);
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  const selectSize = screen.getByLabelText(/select size/i);
+  userEvent.click(addPepperoni);
+  expect(screen.getByText(/small pepperoni/i)).toBeInTheDocument();
+  userEvent.selectOptions(selectSize, "large");
+  expect(screen.getByText(/large pepperoni/i)).toBeInTheDocument();
+});
 
-// "Contact Info" text box
-
-// Submit Order button
+test("form contains a 'Submit Order' button", () => {
+  render(<App />);
+  expect(screen.getByRole("button", { name: /submit order/i })).toBeInTheDocument();
+});
